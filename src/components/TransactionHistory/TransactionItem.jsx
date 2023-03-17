af1cd6414e0a463e8dd5b549b5128efb9d6241d2
KhailoAndrey <hap512@gmail.com>
2023-03-17 15:46:48 +0200

    fix

--- src/components/TransactionHistory/TransactionItem.jsx
+++ src/components/TransactionHistory/TransactionItem.jsx
@@ -13,14 +13,12 @@ const TransactionItem = ({ item: { type, amount, currency } }) => {
 };
 
 TransactionItem.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      type: PropTypes.string.isRequired,
-      amount: PropTypes.string.isRequired,
-      currency: PropTypes.string.isRequired,
-    })
-  ),
+  items: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    amount: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+  }),
 };
 
 export default TransactionItem;
